b1f5f9f8c1c38fc269c69b02633dcbfc4bbcc0be
掌柜的 <passion.yc.cn@gmail.com>
2019-01-28 14:20:46 +0800

    add docker-compose

--- server/config.js
+++ server/config.js
@@ -1,8 +1,7 @@
 module.exports = {
-	db:'mongodb://localhost:27017/app',
-	defaultPort: 3001,
-	staticVirtualPath: '/static',
-	staticRealPath: '/Users/yangmingzhe/Work/opensource/',
-
-	zipReturn: 'diff' // full 全量
+        db:'mongodb://mongo:27017/app',
+        defaultPort: 3001,
+        staticVirtualPath: '/static',
+        staticRealPath: './static',
+        zipReturn: 'diff' // full 全量
 }
